8669121a8844065a6edead06b0ff67f7dac7c384
jvallexm <jvallexm@gmail.com>
2023-01-31 11:27:12 -0500

    fix: output

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -4,7 +4,7 @@
 
 output "vpn_gateway" {
   description = "VPN Gateway information"
-  value       = var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? ibm_is_vpn_gateway.gateway[0] : null
+  value       = var.vpc_id == null ? null : var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? ibm_is_vpn_gateway.gateway[0] : null
 }
 
 ##############################################################################
