406e035694ff0275f452a1dfed8e4358928c69ec
Jennifer Valle <jvallexm@gmail.com>
2023-02-01 04:24:08 -0500

    feat: initial release (#14)

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,20 +1,53 @@
 ##############################################################################
-# Resource Group
+# VPN Gateway
 ##############################################################################
 
-module "resource_group" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
-  # if an existing resource group is not set (null) create a new one using prefix
-  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
-  existing_resource_group_name = var.resource_group
+resource "ibm_is_vpn_gateway" "gateway" {
+  count          = var.vpn_gateway.use_vpn_gateway && var.vpc_id != null ? 1 : 0
+  name           = "${var.prefix}-${var.vpn_gateway.name}"
+  subnet         = var.subnet_id
+  mode           = var.vpn_gateway.mode
+  resource_group = var.resource_group_id
+  tags           = var.tags
+
+  timeouts {
+    delete = "1h"
+  }
 }
 
 ##############################################################################
-# VPC
+
+
+##############################################################################
+# Create Map of Connections
 ##############################################################################
 
-resource "ibm_is_vpc" "vpc" {
-  name           = "${var.prefix}-vpc"
-  resource_group = module.resource_group.resource_group_id
-  tags           = var.resource_tags
+module "vpn_gateway_connection_map" {
+  source = "./list_to_map"
+  list = var.vpc_id == null ? [] : [
+    for connection in(var.vpn_gateway.connections == null ? [] : var.vpn_gateway.connections) :
+    merge(connection, {
+      name = "${var.prefix}-${var.vpn_gateway.name}-${index(var.vpn_gateway.connections, connection) + 1}"
+    })
+  ]
 }
+
+##############################################################################
+
+
+##############################################################################
+# Create Connections
+##############################################################################
+
+resource "ibm_is_vpn_gateway_connection" "gateway_connection" {
+  for_each       = var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? module.vpn_gateway_connection_map.value : {}
+  name           = each.value.name
+  vpn_gateway    = var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? ibm_is_vpn_gateway.gateway[0].id : null
+  peer_address   = each.value.peer_address
+  preshared_key  = each.value.preshared_key
+  local_cidrs    = each.value.local_cidrs
+  peer_cidrs     = each.value.peer_cidrs
+  admin_state_up = each.value.admin_state_up
+}
+
+##############################################################################
--- main.tf
+++ main.tf
@@ -1,3 +1,53 @@
-/********************************************************************
-This file is used to implement the ROOT module.
-*********************************************************************/
+##############################################################################
+# VPN Gateway
+##############################################################################
+
+resource "ibm_is_vpn_gateway" "gateway" {
+  count          = var.vpn_gateway.use_vpn_gateway && var.vpc_id != null ? 1 : 0
+  name           = "${var.prefix}-${var.vpn_gateway.name}"
+  subnet         = var.subnet_id
+  mode           = var.vpn_gateway.mode
+  resource_group = var.resource_group_id
+  tags           = var.tags
+
+  timeouts {
+    delete = "1h"
+  }
+}
+
+##############################################################################
+
+
+##############################################################################
+# Create Map of Connections
+##############################################################################
+
+module "vpn_gateway_connection_map" {
+  source = "./list_to_map"
+  list = var.vpc_id == null ? [] : [
+    for connection in(var.vpn_gateway.connections == null ? [] : var.vpn_gateway.connections) :
+    merge(connection, {
+      name = "${var.prefix}-${var.vpn_gateway.name}-${index(var.vpn_gateway.connections, connection) + 1}"
+    })
+  ]
+}
+
+##############################################################################
+
+
+##############################################################################
+# Create Connections
+##############################################################################
+
+resource "ibm_is_vpn_gateway_connection" "gateway_connection" {
+  for_each       = var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? module.vpn_gateway_connection_map.value : {}
+  name           = each.value.name
+  vpn_gateway    = var.vpn_gateway.use_vpn_gateway == true && var.vpn_gateway.connections != null ? ibm_is_vpn_gateway.gateway[0].id : null
+  peer_address   = each.value.peer_address
+  preshared_key  = each.value.preshared_key
+  local_cidrs    = each.value.local_cidrs
+  peer_cidrs     = each.value.peer_cidrs
+  admin_state_up = each.value.admin_state_up
+}
+
+##############################################################################
